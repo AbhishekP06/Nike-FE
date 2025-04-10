610522455441b3cea4f419760032c5b5a369cdd2
Abhishek Phulari <114561805+AbhishekP06@users.noreply.github.com>
2025-04-10 15:22:46 +0530

    Update spotlightData.js

--- src/app/service/spotlightData.js
+++ src/app/service/spotlightData.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const spotlightApi = createApi({
     reducerPath: "spotlight",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://nike-be.vercel.app"}),
     endpoints: (builder) => ({
         getAllspotlight: builder.query({
             query: () => "/spotlight",
