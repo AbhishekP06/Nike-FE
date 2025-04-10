9832b6a495aee6f025fdcab122c5adf3ebaa8c06
Abhishek Phulari <114561805+AbhishekP06@users.noreply.github.com>
2025-04-10 15:22:25 +0530

    Update productsData.js

--- src/app/service/productsData.js
+++ src/app/service/productsData.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const productsApi = createApi({
     reducerPath: "products",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://nike-be.vercel.app"}),
     endpoints: (builder) => ({
         getAllProducts: builder.query({
             query: () => "/products",
